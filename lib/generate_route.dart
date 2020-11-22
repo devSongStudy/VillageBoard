c7201fa41f9204f40da0d484dfb078695e60cd2a
devSongStudy <ipromf@gmail.com>
2020-11-22 21:32:13 +0900

    기본 화면 플로우

---
+++ lib/generate_route.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:villageboard/src/views/auth/signin_view.dart';
+import 'package:villageboard/src/views/auth/signup_view.dart';
+import 'package:villageboard/src/views/main/detail_view.dart';
+import 'package:villageboard/src/views/main/main_view.dart';
+import 'package:villageboard/src/views/main/write_view.dart';
+import 'package:villageboard/src/views/splash_view.dart';
+
+class GenerateRoute {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    final args = settings.arguments;
+    switch (settings.name) {
+      case '/Splash':
+        return MaterialPageRoute(builder: (_) => SplashView());
+
+      // Auth
+      case '/SignIn':
+        return MaterialPageRoute(builder: (_) => SignInView());
+      case '/SignUp':
+        return MaterialPageRoute(builder: (_) => SignUpView());
+
+      // Main
+      case '/Main':
+        return MaterialPageRoute(builder: (_) => MainView());
+      case '/Detail':
+        return MaterialPageRoute(builder: (_) => DetailView());
+      case '/Write':
+        return MaterialPageRoute(builder: (_) => WriteView());
+
+      default:
+        return MaterialPageRoute(builder: (_) => Scaffold(body: SafeArea(child: Text('Route Error'),),));
+    }
+  }
+}
---
+++ lib/src/views/main/main_view.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:villageboard/src/helpers/app_config.dart' as ex;
+
+class MainView extends StatefulWidget {
+  @override
+  _MainViewState createState() => _MainViewState();
+}
+
+class _MainViewState extends State<MainView> {
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Container(
+                  alignment: Alignment.center,
+                  color: ex.Colors().randomColor(),
+                  child: Text('${this.widget}',
+                    style: TextStyle(fontSize: 28.0),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 4,
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  color: Colors.pinkAccent,
+                  width: double.infinity,
+                  child: Column(
+                    children: [
+                      RaisedButton(
+                        onPressed: showSignInView,
+                        child: Text("Log Out"),
+                      ),
+                      RaisedButton(
+                        onPressed: showDetailView,
+                        child: Text("Detail View"),
+                      ),
+                      RaisedButton(
+                        onPressed: showWriteView,
+                        child: Text("Write View"),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void showSignInView() {
+    try {
+      Navigator.of(context).pushReplacementNamed('/SignIn');
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+
+  void showDetailView() {
+    try {
+      Navigator.of(context).pushNamed('/Detail');
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+
+  void showWriteView() {
+    try {
+      Navigator.of(context).pushNamed('/Write');
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+}
